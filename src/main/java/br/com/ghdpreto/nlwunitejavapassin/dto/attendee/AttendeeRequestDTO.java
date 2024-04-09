479f8c94a440808cfa12facaa7a4f8354a5ff524
Gustavo Henrique <ghdpreto@gmail.com>
2024-04-08 22:51:16 -0300

    Add validacao de campos

--- src/main/java/br/com/ghdpreto/nlwunitejavapassin/dto/attendee/AttendeeRequestDTO.java
+++ src/main/java/br/com/ghdpreto/nlwunitejavapassin/dto/attendee/AttendeeRequestDTO.java
@@ -1,6 +1,15 @@
 package br.com.ghdpreto.nlwunitejavapassin.dto.attendee;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public record AttendeeRequestDTO(
+        @NotNull
+        @NotEmpty
         String name,
+
+        @Email
+        @NotNull
         String email
 ) { }
